a9dc5115359d6bb28c211ac07f3a5162c02a6962
陌 <kevins0526s@gmail.com>
2023-05-03 16:45:32 +0800

    更新 開鎖紀錄

--- app/src/main/java/com/example/qrlockapp/guestKey.java
+++ app/src/main/java/com/example/qrlockapp/guestKey.java
@@ -2,15 +2,23 @@ package com.example.qrlockapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.MediaStore;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,11 +26,12 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class guestKey extends AppCompatActivity {
-    Button backBtn,requestGuestKeyBtn;
+    Button backBtn,requestGuestKeyBtn,shareBtn;
     ImageView guestQrcodeView;
     EditText guestNameEdit;
     String guestName;
@@ -38,6 +47,7 @@ public class guestKey extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest);
         backBtn = findViewById(R.id.back_btn);
+        shareBtn = findViewById(R.id.shareBtn);
         requestGuestKeyBtn = findViewById(R.id.requestGuestKey);
         guestQrcodeView = findViewById(R.id.guestQrcode);
         guestNameEdit = findViewById(R.id.guestName);
@@ -68,24 +78,76 @@ public class guestKey extends AppCompatActivity {
         requestGuestKeyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                requestGuestKeyBtn.setVisibility(View.GONE);
-                guestNameEdit.setVisibility(View.GONE);
-                app.getSwitchGuest(false);
                 String name = guestNameEdit.getText().toString();
-                guestName = "guest_"+name;
-                //countDownTimeTextView.setText(name+"訪客鑰匙到期時間為"+getDateTime());
-                IV=Randomize.IV();
-                aesPassword=AEScbc.encrypt(guestName,String.valueOf(IV));
-                saveName();
-                updateAesPassword(aesPassword,String.valueOf(IV));
-                BarcodeEncoder encoder = new BarcodeEncoder();
-                try {
-                    bit = encoder.encodeBitmap(aesPassword, BarcodeFormat.QR_CODE, 800, 800);
-                    guestQrcodeView.setImageBitmap(bit);
-                } catch (WriterException e) {
-                    e.printStackTrace();
+                if(name.equals("")){
+                    Toast.makeText(guestKey.this, "請輸入訪客名稱喔!", Toast.LENGTH_SHORT).show();
+                }else {
+                    requestGuestKeyBtn.setVisibility(View.GONE);
+                    guestNameEdit.setVisibility(View.GONE);
+                    shareBtn.setVisibility(View.VISIBLE);
+                    app.getSwitchGuest(false);
+                    guestName = "guest_" + name;
+                    IV = Randomize.IV();
+                    aesPassword = AEScbc.encrypt(guestName, String.valueOf(IV));
+                    saveName();
+                    updateAesPassword(aesPassword, String.valueOf(IV));
+                    BarcodeEncoder encoder = new BarcodeEncoder();
+                    try {
+                        bit = encoder.encodeBitmap(aesPassword, BarcodeFormat.QR_CODE, 800, 800);
+                        guestQrcodeView.setImageBitmap(bit);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                    countDownTime();
                 }
-                countDownTime();
+            }
+        });
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bit == null) {
+                    // 如果 bit 為空值，顯示提醒訊息
+                    new AlertDialog.Builder(guestKey.this)
+                            .setTitle("提醒")
+                            .setMessage("沒有 QR Code 圖像可分享")
+                            .setPositiveButton(android.R.string.ok, null)
+                            .show();
+                } else {
+                    // 建立一個分享對象 Intent
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+                    // 設定分享對象的 MIME 類型為圖像
+                    shareIntent.setType("image/*");
+
+                    // 將 Bitmap 圖像加入到分享對象中
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    bit.compress(Bitmap.CompressFormat.PNG, 100, bytes);
+                    String path = MediaStore.Images.Media.insertImage(getContentResolver(), bit, "QR Code", null);
+                    Uri imageUri = Uri.parse(path);
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+
+                    PopupMenu popupMenu = new PopupMenu(guestKey.this, shareBtn);
+                    popupMenu.getMenuInflater().inflate(R.menu.share_menu, popupMenu.getMenu());
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.line:
+                                    // 呼叫 LINE 應用程式的分享功能
+                                    shareIntent.setPackage("jp.naver.line.android");
+                                    startActivity(shareIntent);
+                                    break;
+                                case R.id.messenger:
+                                    // 執行 Messager 分享功能
+                                    shareIntent.setPackage("com.facebook.orca");
+                                    startActivity(shareIntent);
+                                    break;
+                            }
+                            return true;
+                        }
+                    });
+                    popupMenu.show();
+                }
             }
         });
     }
@@ -114,6 +176,7 @@ public class guestKey extends AppCompatActivity {
                 deleteAesPassword(aesPassword);
                 clear();
                 countDownTimeTextView.setText("可以新增訪客鑰匙囉~");
+                shareBtn.setVisibility(View.GONE);
                 requestGuestKeyBtn.setEnabled(true);
                 app.getSwitchGuest(true); //時間過後再進才能重新生成
             }
